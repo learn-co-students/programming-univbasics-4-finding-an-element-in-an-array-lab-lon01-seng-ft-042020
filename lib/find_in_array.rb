b870629fcd1284c3c805bd115bb3543ea0e12e24
Dorothy John <dorothyy.john@gmail.com>
2020-04-12 18:38:28 +0000

    Done.

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,14 @@
+
+#Return the index of the value that was passes
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  
+  if  array.include?(value_to_find)
+      array.length.times { |i|
+     if array[i] == value_to_find
+         return i
+       end
+      }
+ else puts "#{value_to_find} is not in our array"
+ end
 end
+
